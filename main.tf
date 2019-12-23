453977b50f8af234b1099bc9a2508cfee95684c2
azuresamples <54381444+azuresamples@users.noreply.github.com>
2019-12-23 15:32:35 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,13 @@ data "azurerm_resource_group" "test" {
 data "azurerm_storage_account" "test" {
   name = "sugaeastasiarsgdiag"
 }
+
+data "azurerm_virtual_network" "test" {
+  name = "suga-eastasia-vnet"
+}
+
+data "azurerm_subnet" "test" {
+  name                 = "default"
+  resource_group_name  = "${azurerm_resource_group.test.name}"
+  virtual_network_name = "${azurerm_virtual_network.test.name}"
+}
